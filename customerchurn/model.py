5de0457755fd833f1625641ffdf2c6e0105a9f6a
Alen Chen <alechenwolf@gmail.com>
2022-02-12 17:31:00 +1100

    add bert and sth about trainer.py

--- customerchurn/model.py
+++ customerchurn/model.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text as text
-from official.nlp import optimization
+# from tensorflow.keras.optimizers import Adam
 
 
 tfhub_handle_preprocess = 'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3'
 tfhub_handle_encoder = 'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1'
 
-def build_classifier_model():
+def build_classifier_model(lr):
     text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
     preprocessing_layer = hub.KerasLayer(tfhub_handle_preprocess, name='preprocessing')
 
@@ -20,29 +20,36 @@ def build_classifier_model():
     net = tf.keras.layers.Dropout(0.1)(net)
     net = tf.keras.layers.Dense(1, activation='sigmoid', name='classifier')(net)
 
-    return tf.keras.Model(text_input, net)
+    model = tf.keras.Model(text_input, net)
+    model.compile(loss='binary_crossentropy',
+                  optimizer=tf.keras.optimizers.Adam(learning_rate=lr),
+                  metrics=['accuracy'])
+
+    return model
 
 
-def get_model(model_name):
 
-    if model_name == "RNN-V1":
 
-        # model_params = dict(
-        #   )
+# def get_model(model_name):
 
-        # model = RandomForestRegressor()
-        # model.set_params(**model_params)
+#     if model_name == "RNN-V1":
 
-        # return model
-        pass
+#         # model_params = dict(
+#         #   )
 
-    elif model_name == "RNN-V2":
+#         # model = RandomForestRegressor()
+#         # model.set_params(**model_params)
 
-        # model_params = dict(
-        #   )
+#         # return model
+#         pass
 
-        # model = RandomForestRegressor()
-        # model.set_params(**model_params)
+#     elif model_name == "RNN-V2":
 
-        # return model
-        pass
+#         # model_params = dict(
+#         #   )
+
+#         # model = RandomForestRegressor()
+#         # model.set_params(**model_params)
+
+#         # return model
+#         pass
--- customerchurn/trainer.py
+++ customerchurn/trainer.py
@@ -1,18 +1,25 @@
-from customerchurn.data import get_data, clean_df, holdout
-from customerchurn.model import get_model
-from customerchurn.pipeline import get_pipeline
-from customerchurn.mlflow import MLFlowBase
+from customerchurn.data import get_data
+from customerchurn.model import build_classifier_model
+# from customerchurn.pipeline import get_pipeline
+from customerchurn.mlflowlog import MLFlowBase
 from customerchurn.params import MLFLOW_URI, EXPERIMENT_NAME
 
 import joblib
 
+from sklearn.model_selection import train_test_split, GridSearchCV
+from keras.wrappers.scikit_learn import KerasClassifier
+import tensorflow as tf
+# from tensorflow.keras.callbacks import EarlyStopping
+
 
 class Trainer(MLFlowBase):
 
-    def __init__(self):
+    def __init__(self, lr=[0.001], batch_sizes=[16, 32, 256]):
         super().__init__(
             EXPERIMENT_NAME,
             MLFLOW_URI)
+        self.learning_rate = lr
+        self.batch_sizes = batch_sizes
 
     def train(self):
 
@@ -26,35 +33,63 @@ class Trainer(MLFlowBase):
 
         # get data
         df = get_data(path = './customerchurn/data/concat.csv')
-        df = clean_df(df)
 
-        # holdout
-        X_train, X_test, y_train, y_test = holdout(df)
+        # get x, y
+        X = df['review']
+        y = df['recommendation'].apply(lambda x: 0
+                                            if x == 'Not Recommended' else 1)
 
-        # log params
-        self.mlflow_log_param("model", model_name)
+        # train test split
+        X_train, X_test, y_train, y_test = train_test_split(X,
+                                                            y,
+                                                            test_size=0.33,
+                                                            random_state=42)
 
         # create model
-        model = get_model(model_name)
+        es = tf.keras.callbacks.EarlyStopping(
+            monitor='val_loss', restore_best_weights=True, patience=4)
 
-        # create pipeline
-        pipeline = get_pipeline(model)
+        model = KerasClassifier(build_fn=build_classifier_model,
+                                validation_split=0.2,
+                                callbacks=[es])
+        # KerasRegressor(build_fn=baseline_model_v2,
+        #                verbose=1,
+        #                epochs=1000,
+        #                batch_size=128,
+        #                shuffle=True,
+        #                validation_split=0.2,
+        #                callbacks=[es])
+
+        # # log params
+        # self.mlflow_log_param("learning_rate", self.learning_rate)
+
+        # # create pipeline
+        # pipeline = get_pipeline(model)
+        learning_rate = self.learning_rate
+        batch_size = self.batch_sizes
+        param_grid = dict(batch_size=batch_size, lr=learning_rate)
+        grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=5)
 
         # train
-        pipeline.fit(X_train, y_train)
+        model.fit(X_train, y_train)
 
         # make prediction for metrics
-        y_pred = pipeline.predict(X_test)
+        # y_pred = pipeline.predict(X_test)
 
         # evaluate metrics
         #SCORING FUNCTION
         #score = compute_rmse(y_pred, y_test)
 
         # save the trained model
-        joblib.dump(pipeline, "model.joblib")
+        joblib.dump(model, "model.joblib")
 
         # push metrics to mlflow
-        self.mlflow_log_metric("score", score)
+        # self.mlflow_log_metric("score", score)
 
         # return the gridsearch in order to identify the best estimators and params
-        return pipeline
+        return model
+
+if __name__ == '__main__':
+
+    model = Trainer()
+    model.train()

974e491c708a231b381cd5989b1424da3464e923
Cathal OConnor <cathal.oconnor56@gmail.com>
2022-02-17 19:41:59 +1100

    ignore the models

--- api/fast.py
+++ api/fast.py
@@ -20,7 +20,7 @@ app.add_middleware(
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
-new_model = tf.keras.models.load_model('sentiment_model1')
+new_model = tf.keras.models.load_model('models/sentiment_model1')
 
 @app.get("/")
 def index():
@@ -30,7 +30,7 @@ def index():
 @app.post("/predict")
 def upload_file(file: UploadFile = File(...)):
     X_pred  = pd.read_csv(file.file)['review']
-    # new_model = tf.keras.models.load_model('sentiment_model1') better off outside so not reloaded for each call
+    # new_model = tf.keras.models.load_model('models/sentiment_model1') better off outside so not reloaded for each call
     prediction = new_model.predict(X_pred)
     dict_pred = {'pred':[str(x) for x in list(prediction.reshape(-1))]}
     print(dict_pred)

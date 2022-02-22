f7b2aea210b8a505eb1fa5423874356b8697ff85
Alen Chen <alechenwolf@gmail.com>
2022-02-23 00:23:09 +1100

    aa

--- customerchurn/gcp.py
+++ customerchurn/gcp.py
@@ -4,17 +4,21 @@ from google.cloud import storage
 from termcolor import colored
 from customerchurn.params import BUCKET_NAME, MODEL_NAME, MODEL_VERSION
 
-_file1 = 'my_model/assets/vocab.txt'
-_file2 = 'my_model/variables/variables.data-00000-of-00001'
-_file3 = 'my_model/variables/variables.index'
-_file4 = 'my_model/keras_metadata.pb'
-_file5 = 'my_model/saved_model.pb'
+# _file1 = 'my_model/assets/vocab.txt'
+# _file2 = 'my_model/variables/variables.data-00000-of-00001'
+# _file3 = 'my_model/variables/variables.index'
+# _file4 = 'my_model/keras_metadata.pb'
+# _file5 = 'my_model/saved_model.pb'
+
+file_path = []
+for parent, dirnames, filenames in os.walk('my_model'):
+    for filename in filenames:
+        file_path.append(parent + '/' + filename)
 
 
 def storage_upload(rm=False):
     client = storage.Client().bucket(BUCKET_NAME)
-
-    for local_model_name in [_file1,_file2, _file3, _file4, _file5]:
+    for local_model_name in file_path:
         storage_location = f"models/{MODEL_NAME}/{MODEL_VERSION}/{local_model_name}"
         blob = client.blob(storage_location)
         blob.upload_from_filename(local_model_name)
